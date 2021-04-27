3103e793f89078f4db5738489be2489a9b3e875c
Isa Daud <isadaud2017@gmail.com>
2021-04-27 14:27:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/uas_daud/Home.java
@@ -0,0 +1,100 @@
+package com.example.uas_daud;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Home extends AppCompatActivity {
+
+    TextView hoby,hobbi;
+    TextInputEditText nama;
+    RequestQueue queue,mque;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        queue = Volley.newRequestQueue(this);
+        mque = Volley.newRequestQueue(this);
+        hoby = findViewById(R.id.hobi);
+        nama = findViewById(R.id.namaInp);
+        hobbi = findViewById(R.id.hobis);
+
+    }
+
+    public void data(View view) {
+        String URL = "http://192.168.5.219/UAS_daud/show.php";
+        String userNama = nama.getText().toString();
+
+        StringRequest request = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.contains("kegemaran")){
+                            Toast.makeText(Home.this, "Ada namanya"+response, Toast.LENGTH_SHORT).show();
+                            JsonObjectRequest objectRequest = new JsonObjectRequest(Request.Method.GET,
+                                    "http://192.168.5.219/MPA/jsondb/show.php", null, new Response.Listener<JSONObject>() {
+                                @Override
+                                public void onResponse(JSONObject response) {
+                                    try {
+                                        JSONArray array = response.getJSONArray("kegemaran");
+                                        for (int i = 0; i < array.length(); i++){
+                                            JSONObject mahasantri = array.getJSONObject(i);
+                                            String gemar = mahasantri.getString("nama_hobby");
+                                            hobbi.append(gemar+"\n");
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }, new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    Toast.makeText(Home.this, "erro :"+error.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                            mque.add(objectRequest);
+                        }else {
+                            Toast.makeText(Home.this, "Nama tidak terdaftar"+response, Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Home.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("nama",userNama);
+                return params;
+            }
+        };
+        queue.add(request);
+
+    }
+}
---
+++ app/src/main/java/com/example/uas_daud/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.uas_daud;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextInputEditText name,passwords;
+    RequestQueue queue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.nama);
+        passwords = findViewById(R.id.password);
+        queue = Volley.newRequestQueue(this);
+
+
+
+    }
+
+    public void login(View view) {
+        String URL = "http://192.168.5.219/UAS_daud/login.php";
+        String userNama = name.getText().toString();
+        String userPassword = passwords.getText().toString();
+
+        StringRequest request = new StringRequest(Request.Method.POST, URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        if (response.contains(userNama)){
+                            startActivity(new Intent(MainActivity.this,Home.class));
+                            Toast.makeText(MainActivity.this, "Login Suucess"+response, Toast.LENGTH_SHORT).show();
+                        }else {
+                            Toast.makeText(MainActivity.this, "Login Failed"+response, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this, "Gagal"+error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> params = new HashMap<>();
+                params.put("nama",userNama);
+                params.put("password",userPassword);
+                return params;
+            }
+        };
+        queue.add(request);
+
+    }
+}
